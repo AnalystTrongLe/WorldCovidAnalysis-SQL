705b9c59822fbea75334faf2e2998edfe7aea00f
Analyst Trong Le <156616865+AnalystTrongLe@users.noreply.github.com>
2024-01-16 22:46:30 -0500

    Update WorldCovidAnalysis_Part1_Intro_and_DataPreparation.sql
    + Added new variables to improve the "excess mortality" calculation in the analysis section
    + Removed various "expected" to reduce confusion as it was primarily inverse of the excess values
    + Modified filters in the data processing to further reduce outliers

--- WorldCovidAnalysis_Part1_Intro_and_DataPreparation.sql
+++ WorldCovidAnalysis_Part1_Intro_and_DataPreparation.sql
@@ -41,7 +41,7 @@ SELECT continent, location, date,
 	total_cases, new_cases, total_deaths, new_deaths,
 	total_vaccinations, new_vaccinations, people_vaccinated, people_fully_vaccinated,
 	population, hosp_patients, excess_mortality, excess_mortality_cumulative, excess_mortality_cumulative_absolute
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 WHERE location = 'World'
 ORDER BY date;
 
@@ -52,7 +52,7 @@ SELECT COUNT(continent) AS continent, COUNT(location) AS location, COUNT(date) A
 	COUNT(total_cases) AS totCases, COUNT(new_cases) AS newCases, COUNT(total_deaths) AS totDeaths, COUNT(new_deaths) AS newDeaths,
 	COUNT(total_vaccinations) AS totVac, COUNT(new_vaccinations) AS newVac, COUNT(people_vaccinated) AS peopVac, COUNT(people_fully_vaccinated) AS peopFulVac,
 	COUNT(population) AS pop, COUNT(hosp_patients) AS hospPatients, COUNT(excess_mortality) AS excMortality, COUNT(excess_mortality_cumulative) excMorCumulative, COUNT(excess_mortality_cumulative_absolute) AS excDeaths
-FROM owid_covid_data_20240110;
+FROM PortfolioProjects..owid_covid_data_20240110;
 /*NOTE:
 1) Location, Date, and Population are the most reliable variables based on completeness.
 2) Continent data is not complete. [Requires Investigation]
@@ -66,7 +66,7 @@ FROM owid_covid_data_20240110;
 
 --Investigation Continents
 SELECT DISTINCT continent, location
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 ORDER BY 1;
 --Result: Continent is NULL when Location contains continent values.
 
@@ -77,7 +77,7 @@ SELECT continent, location, date,
 	total_cases, new_cases, total_deaths, new_deaths,
 	total_vaccinations, new_vaccinations, people_vaccinated, people_fully_vaccinated,
 	population, hosp_patients, excess_mortality, excess_mortality_cumulative, excess_mortality_cumulative_absolute
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 WHERE location = 'World';
 --Result: Location 'World' only contains COVID-19 cases, deaths, and vaccinations. Patients hospitalized and all three excess mortality rates are null.
 --NOTE: The dataset with Location of 'World' is cumulated by Our World in Data from countries' reportings.
@@ -86,7 +86,7 @@ WHERE location = 'World';
 
 --Investigation World's COVID-19 cases and deaths
 SELECT SUM(new_cases) sumNewCases, MAX(CAST(total_cases AS float)) maxTotCases, SUM(new_deaths) sumNewDeaths, MAX(CAST(total_deaths AS float)) maxTotDeaths
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 WHERE location = 'World';
 --Result: The total for new cases and new deaths are slightly off from the max of Total Cases and Total deaths but within acceptable margin of error.
 
@@ -94,7 +94,7 @@ WHERE location = 'World';
 
 --Investigation World's Vaccinations
 SELECT SUM(CAST(new_vaccinations AS float)) sumNewVac, MAX(CAST(total_vaccinations AS float)) maxTotVac, MAX(CAST(people_vaccinated AS float)) pepVac, MAX(CAST(people_fully_vaccinated AS float)) pepFulVac
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 WHERE location = 'World';
 --Result: The total for new vaccinations is slightly less than the max of Total Vaccination but within acceptable margin of error.
 
@@ -102,11 +102,11 @@ WHERE location = 'World';
 
 --Investigation reported Hospitalized Patients
 SELECT APPROX_COUNT_DISTINCT(location) uniqueCountries
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 WHERE hosp_patients IS NOT NULL;
 
 SELECT date, COUNT(hosp_patients) numReports
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 GROUP BY date
 HAVING COUNT(hosp_patients) > 0
 ORDER BY 2;
@@ -114,23 +114,25 @@ ORDER BY 2;
 WITH filtered_hosp_pat
 (date, numReports) AS (
 	SELECT date, COUNT(hosp_patients)
-	FROM owid_covid_data_20240110
+	FROM PortfolioProjects..owid_covid_data_20240110
 	GROUP BY date
 	HAVING COUNT(hosp_patients) > 0)
-SELECT AVG(numReports) AS avgReports, STDEVP(numReports) * 2 AS 'within_95%_tile', AVG(numReports) - STDEVP(numReports) * 2 AS lowest_accepted_reportings
+SELECT AVG(numReports) AS avgReports, STDEVP(numReports) * 1.3 AS 'within_90%_tile',
+	AVG(numReports) - STDEVP(numReports) * 1.3 AS lowest_accepted_reportings,
+	AVG(numReports) + STDEVP(numReports) * 1.3 AS highest_accepted_reportings
 FROM filtered_hosp_pat;
---Result: Only 40 unique countries reported hospitalization by COVID-19. A filter of 9 or more reports per date should capture 95% of the data while reducing outliers.
---NOTE: Filter of at least 10 reports per date will removes 66 dates or 4.7%.
+--Result: Only 40 unique countries reported hospitalization by COVID-19. A filter of 15 to 38 reports per date should capture 90% of the data while reducing outliers.
+--NOTE: A filter of 15 to 38 reports per date will removes 217 or 15.3% of dates.
 
 ------------------------------------------------------------------------------
 
 --Investigation reported Excess Mortality
 SELECT APPROX_COUNT_DISTINCT(location) uniqueCountries
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 WHERE excess_mortality IS NOT NULL;
 
 SELECT date, COUNT(excess_mortality) numReports
-FROM owid_covid_data_20240110
+FROM PortfolioProjects..owid_covid_data_20240110
 GROUP BY date
 HAVING COUNT(excess_mortality) > 0
 ORDER BY 2;
@@ -138,10 +140,12 @@ ORDER BY 2;
 WITH filtered_exc_mort
 (date, numReports) AS (
 	SELECT date, COUNT(excess_mortality)
-	FROM owid_covid_data_20240110
+	FROM PortfolioProjects..owid_covid_data_20240110
 	GROUP BY date
 	HAVING COUNT(excess_mortality) > 0)
-SELECT AVG(numReports) AS avgReports, STDEVP(numReports) * 2 AS 'within_95%_tile', AVG(numReports) - STDEVP(numReports) * 2 AS suggestedFilter
+SELECT AVG(numReports) AS avgReports, STDEVP(numReports) * 1.3 AS 'within_90%_tile',
+	AVG(numReports) - STDEVP(numReports) * 1.3 AS lowest_accepted_reportings,
+	AVG(numReports) + STDEVP(numReports) * 1.3 AS highest_accepted_reportings
 FROM filtered_exc_mort;
---Result: Only 124 unique countries reported excess mortality. A filter of 20 or more reports per date should capture about 95% of the data while reducing outliers.
---NOTE: Filter of at least 20 reports per date will removes 12 dates or 5.1%.
+--Result: Only 124 unique countries reported excess mortality. A filter of 31 to 69 reports per date should capture about 90% of the data while reducing outliers.
+--NOTE: A filter of 31 to 69 reports per date will removes 21 dates or 8.9%.
