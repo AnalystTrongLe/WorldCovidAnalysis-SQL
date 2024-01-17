b79011fcce3164284d91f445db634e9862272657
Analyst Trong Le <156616865+AnalystTrongLe@users.noreply.github.com>
2024-01-16 22:51:11 -0500

    Update WorldCovidAnalysis_Part4_DataVisualization_ReducedComments.sql
    + Added new variables to improve the "excess mortality" calculation in the analysis section
    + Removed various "expected" to reduce confusion as it was primarily inverse of the excess values
    + Modified filters in the data processing to further reduce outliers

--- Reduce_Comments_Queries/WorldCovidAnalysis_Part4_DataVisualization_ReducedComments.sql
+++ Reduce_Comments_Queries/WorldCovidAnalysis_Part4_DataVisualization_ReducedComments.sql
@@ -2,6 +2,7 @@
 
 --============================================================================
 
+/* Data Visualizations */
 --Creating a permanent table because CREATE VIEW does not work with temp tables
 DROP TABLE IF EXISTS tableauWorldCovidData;
 CREATE TABLE tableauWorldCovidData
@@ -9,21 +10,18 @@ CREATE TABLE tableauWorldCovidData
 	population float, total_cases float, new_cases float, total_covid_deaths float, new_covid_deaths float,
 	total_vaccinations float, new_vaccinations float, people_vaccinated float, people_fully_vaccinated float,
 	excess_mortality float, excess_cumulative_mortality float, excess_deaths float,
-	prevalence_rate float, fatality_rate float, incidence_rate_per_1mil float, mortality_rate_per_1mil float,
-	covid_mortality_rate float, expected_mortality float, covid_cumulative_mortality_rate float,
-	expected_cumulative_mortality_rate float, expected_deaths float);
+	total_deaths_by_excess_mortality float, new_deaths_by_excess_mortality float, population_by_excess_mortality float,
+	prevalence_rate float, fatality_rate float, incidence_rate float, mortality_rate float,
+	covid_mortality_rate float, covid_cumulative_mortality_rate float);
 
 INSERT INTO tableauWorldCovidData
 SELECT *,
 	total_cases/population * 100 AS prevalence_rate,
 	total_deaths/population * 100 AS fatality_rate,
-	new_cases/population * 1000000 AS incidence_rate_per_1mil,
-	new_deaths/population * 1000000 AS mortality_rate_per_1mil,
-	new_deaths/population * 100 AS covid_mortality_rate,
-	(new_deaths * 100 / population) - excess_mortality AS expected_mortality,
-	total_deaths/population * 100 AS covid_cumulative_mortality_rate,
-	(total_deaths * 100 / population) - excess_mortality_cumulative AS expected_cumulative_mortality_rate,
-	total_deaths - excess_mortality_cumulative_absolute AS expected_deaths
+	new_cases/population * 100 AS incidence_rate,
+	new_deaths/population * 100 AS mortality_rate,
+	new_deaths_by_excess_mortality/population_by_excess_mortality * 100 AS covid_mortality_rate,
+	total_deaths_by_excess_mortality/population_by_excess_mortality * 100 AS covid_cumulative_mortality
 FROM #ProcessedCovidData;
 
 --Creating an exportable dataset for Tableau
@@ -68,21 +66,19 @@ ORDER BY 2;
 
 --Determining the top 5 reports with the highest excess deaths
 SELECT TOP 5
-	date, total_deaths AS covid_deaths,
-	total_deaths - excess_mortality_cumulative_absolute AS expected_deaths,
-	excess_mortality_cumulative_absolute AS excess_deaths
+	date, total_deaths_by_excess_mortality AS total_covid_deaths,
+	excess_mortality_cumulative_absolute AS total_excess_deaths
 FROM #ProcessedCovidData
 WHERE total_deaths IS NOT NULL OR excess_mortality_cumulative_absolute IS NOT NULL
-ORDER BY 4 DESC;
+ORDER BY 3 DESC;
 
 --Determining when was the lowest excess deaths
 SELECT TOP 5
-	date, total_deaths AS covid_deaths,
-	total_deaths - excess_mortality_cumulative_absolute AS expected_deaths,
-	excess_mortality_cumulative_absolute AS excess_deaths
+	date, total_deaths_by_excess_mortality AS total_covid_deaths,
+	excess_mortality_cumulative_absolute AS total_excess_deaths
 FROM #ProcessedCovidData
 WHERE total_deaths IS NOT NULL AND excess_mortality_cumulative_absolute IS NOT NULL
-ORDER BY 4;
+ORDER BY 3;
 
 --Determining the top 5 reports with the highest new vaccinations
 SELECT TOP 5
